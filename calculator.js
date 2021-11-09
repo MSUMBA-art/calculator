2080c0b001f9464de515c616318562054685e7fd
ALEXANDER <msumba83@gmail.com>
2021-11-09 10:55:31 +0200

    Calculator Setup

--- calculator.js
+++ calculator.js
@@ -1,4 +1,13 @@
 const express = require("express");
 
-// making a web app
-express
+const app = express();
+
+app.get("/", function(req, res) {
+      res.send("<h1>HELLO WORLD</h1>");
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
+
